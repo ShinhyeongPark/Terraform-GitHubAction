07ce831c5911f6a79b6e43f9e33e859be2e640c0
ShinhyeongPark <orpsh1941@gmail.com>
2021-10-29 09:51:40 +0900

    Little Update

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,13 +3,110 @@ variable "region" {
 }
 
 variable "vpc_name" {
-    default = "vpc-demo-shpark"
+    default = "vpc-shpark"
 }
 
 variable "vpc_cidr_block" {
     default = "10.80.0.0/16"
 }
 
+variable "vpc_tags" {
+  default = {
+    Name = "vpc-shpark"
+  }
+
+}
+
+variable "public_subnet" {
+    default = ["10.80.11.0/24", "10.80.12.0/24"]
+}
+
+variable "private_subnet" {
+    default = ["10.80.21.0/24", "10.80.22.0/24"]
+}
+
+variable "db_subnet" {
+    default = ["10.80.31.0/24", "10.80.32.0/24"]
+}
+
+variable "igw_tags" {
+    default = {
+        Name = "igw-public-1a-shpark"
+    }
+}
+
+variable "nat_eip_tags" {
+    default = {
+        Name = eip-shpark
+    }
+}
+
+variable "nat_gateway_tags" {
+    default = {
+        Name = 
+    }
+}
+
+# NAT 게이트웨이 관련 설정
+variable "nat_gateway_single" {
+  default = true
+}
+
+variable "nat_gateway_enable" {
+  default = true
+}
+
+variable "nat_gateway_one_per_az" {
+  default = false
+}
+
+variable "nat_gateway_ips_reuse" {
+  default = true
+}
+
+variable "route_table_public_tags" {
+    default = {
+        Name = "rt-public-shpark"
+    }
+}
+
+variable "route_table_private_tags" {
+    default = {
+        Name = "rt-private-shpark"
+    }
+}
+
+variable "route_table_db_tags" {
+    default = {
+        Name = "rt-db-shpark"
+    }
+}
+
+variable "public_subnet_tags" {
+    default = {
+        Name = "subnet-public-shpark"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {
+        Name = "subnet-private-shpark"
+    }
+}
+
+variable "db_subnet_tags" {
+    default = {
+        Name = "subnet-db-shpark"
+    }
+}
+
+variable "enable_dns_host" {
+    default = true
+}
+
+variable "enable_dns_support" {
+    default = true
+}
 # variable "bastion-rt-subnet" {
 #     default = [
 #         "10.80.11.0/24",
