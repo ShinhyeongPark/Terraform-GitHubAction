8da04f65d9150dc2c4054cc79d47b556f9ae5f7b
ShinhyeongPark <orpsh1941@gmail.com>
2021-10-29 16:48:10 +0900

    Create EC2(Bastion) Success!

---
+++ variables-app.tf
@@ -0,0 +1,11 @@
+# variable "app_key" {
+#     default = "secret"
+# }
+
+# variable "alb_http_sg_name" {
+#     default = "alb-http-sg-shpark"
+# }
+
+# variable "alb_http_sg_tags" {
+#     default = "alb-http-sg-shpark"
+# }
